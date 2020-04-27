337211a7e4516cb137e72f4693f4673456ac0a68
Johny Herangi <johnyherangi@gmail.com>
2020-04-27 19:50:03 +1200

    add puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello, World!")
+import puppeteer, { DirectNavigationOptions } from "puppeteer"
+
+const navigationOptions: DirectNavigationOptions = {
+    waitUntil: "networkidle2",
+    timeout: 60000,
+}
+
+const main = async () => {
+    const browser = await puppeteer.launch({ headless: false })
+    const pages = await browser.pages()
+    const page = pages[0]
+    await page.goto("https://secure2.ipayroll.co.nz/login", navigationOptions)
+
+    await page.type("#username", "test")
+    await page.type("#password", "test")
+    await page.click("#btn-login")
+    await page.waitForNavigation(navigationOptions)
+
+    await browser.close()
+}
+
+main()
